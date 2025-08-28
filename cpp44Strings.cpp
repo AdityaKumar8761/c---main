fc09bca512d7479ee4f745b541d17fa68f587d09
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2025-08-28 19:55:12 +0530

    r

---
+++ cpp44Strings.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+int main(){
+//     char str[] = "hello"; //string literal
+
+//     cout << strlen(str) << endl;
+
+//     char str2[100];
+//     cin.getline(str2,100,'$');
+
+//     cout << str2<<endl;
+
+
+//     char str3[] = "hello world i am aditya";
+//     int len = 0;
+
+//     for(int i=0; i<str3[i] != '\0' ; i++){
+//         len ++;
+//     }
+//     cout << len << endl;
+
+//     string str4 ="hello worl";  //dynamic in nautre 
+//     cout << str4 << endl;
+
+    string str5 ;
+    getline(cin,str5);
+    cout << str5 << endl;
+    return 0;
+
+
+
+}
