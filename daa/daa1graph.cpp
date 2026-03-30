cbfdcbb9da03da970800a073e1957cc614f84379
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2026-03-30 18:48:55 +0530

    graph and graph traversel(bfs and dfs)

---
+++ daa/daa1graph.cpp
@@ -0,0 +1,51 @@
+// types of graph( connected and disconnected)
+// -> undir weighted graph
+// -> undir unweighted graph
+// -> dir weighted graph
+// -> dir unweighted graph
+
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Graph{
+    int v;
+    list<int>*l;  //int *arr dynamic arr
+
+public:
+    Graph(int v){
+        this->v = v;
+        l = new list<int>[v];
+        //arr = new int[v]
+    }
+
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void printAdjList(){
+        for(int i=0; i<v ; i++){
+            cout << i << " :" ;
+            for(int neigh : l[i]){
+                cout << neigh << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+
+    g.printAdjList();
+
+    return 0;
+}
---
+++ daa/daa2bfs.cpp
@@ -0,0 +1,90 @@
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Graph{
+    int v;
+    list<int>*l;  //int *arr dynamic arr
+
+public:
+    Graph(int v){
+        this->v = v;
+        l = new list<int>[v];
+        //arr = new int[v]
+    }
+
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void printAdjList(){
+        for(int i=0; i<v ; i++){
+            cout << i << " :" ;
+            for(int neigh : l[i]){
+                cout << neigh << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void BFS(int v){ //O(v+e)
+        queue<int> q;
+        vector<bool> visited(this->v , false);
+
+        q.push(v);
+        visited[v] = true;
+
+        while(!q.empty()){
+            int d = q.front();
+            q.pop();
+
+            cout << d << " ";
+
+            for(int n : l[d]){
+                if(!visited[n]){
+                    q.push(n);
+                    visited[n] = true;
+                }
+            }
+        }
+    }
+
+    void DFSunit(int v , vector<bool> &vis){
+        cout << v << " ";
+        vis[v] = true;
+         
+        for(int u : l[v]){
+            if(!vis[u]){
+                DFSunit(u, vis);
+            }
+        }
+    }
+
+    void DFS(int v){  //O(v+e)
+        vector<bool> vis(this->v , false);
+        DFSunit(v , vis );
+
+    }
+};
+
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+
+    g.printAdjList(); cout << endl;
+
+    g.BFS(0); cout <<endl;
+
+    g.DFS(0);
+
+    return 0;
+}
